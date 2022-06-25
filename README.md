a838f237d7b86ed40543b39a6a3ac2d695c04bc3
Agboola Vincent <agboolavin65@gmail.com>
2022-06-25 10:05:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction to Javascript
+
+## [Link to the website]()
---
+++ app.js
@@ -0,0 +1,7 @@
+var name = 'Agboola Vincent';
+var height = '5.2 ft';
+var country = 'Nigeria';
+
+document.write('My name is ', name, ',');
+document.write('<br> My height is ', height, ',');
+document.write('<br> I am from ', country, '.');
